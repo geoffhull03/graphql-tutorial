9a3ea0604c0a8352ff2bff904be33a70a3ccf1ec
Geoff Hull <geoffhull03@gmail.com>
2018-07-31 14:28:13 -0700

    Links can be created and associated with a User

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -4,19 +4,32 @@ class GraphqlController < ApplicationController
     query = params[:query]
     operation_name = params[:operationName]
     context = {
-      # Query context goes here, for example:
-      # current_user: current_user,
+      # we need to provide session and current user
+      session: session,
+      current_user: current_user
     }
     result = GraphqlTutorialSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+
     render json: result
-  rescue => e
-    raise e unless Rails.env.development?
-    handle_error_in_development e
   end
 
   private
 
-  # Handle form data, JSON body, or a blank value
+  # gets current user from token stored in session
+  def current_user
+    # if we want to change the sign-in strategy, this is the place todo it
+    return unless session[:token]
+
+    # For Ruby on Rails >=5.2.x use:
+    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
+    
+    token = crypt.decrypt_and_verify session[:token]
+    user_id = token.gsub('user-id:', '').to_i
+    User.find_by id: user_id
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    nil
+  end
+
   def ensure_hash(ambiguous_param)
     case ambiguous_param
     when String
--- app/graphql/resolvers/sign_in_user.rb
+++ app/graphql/resolvers/sign_in_user.rb
@@ -9,22 +9,20 @@ class Resolvers::SignInUser < GraphQL::Function
     field :user, Types::UserType
   end
 
-  def call(_obj, args, _ctx)
+  def call(_obj, args, ctx)
     input = args[:email]
 
-    # basic validation
     return unless input
 
     user = User.find_by email: input[:email]
 
-    # ensures we have the correct user
     return unless user
     return unless user.authenticate(input[:password])
 
-    # use Ruby on Rails - ActiveSupport::MessageEncryptor, to build a token
-    # For Ruby on Rails >=5.2.x use:
     crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
     token = crypt.encrypt_and_sign("user-id:#{ user.id }")
+
+    ctx[:session][:token] = token
 
     OpenStruct.new({
       user: user,
